181ca7f9c7e5dc31b4243228dc45fafc4cc58f41
Dave Vallée <davevallee27@gmail.com>
2025-12-18 20:03:01 +0000

    File changes

--- src/pages/PriseDeMandat.jsx
+++ src/pages/PriseDeMandat.jsx
@@ -2198,7 +2198,7 @@ export default function PriseDeMandat() {
                     {formData.statut === "Mandats à ouvrir" && (
                       <Button
                         type="button"
-                        className="bg-gradient-to-r from-purple-500 to-purple-600 hover:from-purple-600 hover:to-purple-700 border-2 border-purple-500"
+                        className="bg-gradient-to-r from-purple-500 to-purple-600 hover:from-purple-600 hover:to-purple-700 border-2 border-purple-500 text-purple-300"
                         onClick={() => {
                           // Utiliser le numéro de dossier déjà attribué
                           const prochainNumero = formData.numero_dossier;
