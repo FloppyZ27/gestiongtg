304bef572a60ea62eb12407b212cfeda1ec9a091
Dave Vallée <davevallee27@gmail.com>
2025-12-18 20:31:43 +0000

    File changes

--- src/pages/PriseDeMandat.jsx
+++ src/pages/PriseDeMandat.jsx
@@ -4067,7 +4067,7 @@ export default function PriseDeMandat() {
                  </Dialog>
 
                  {/* Table des prises de mandat */}
-        <Card className="border-slate-800 bg-slate-900/50 backdrop-blur-xl shadow-xl">
+                 <Card className="border-slate-800 bg-slate-900/50 backdrop-blur-xl shadow-xl">
           <CardHeader className="border-b border-slate-800">
             <div className="flex flex-col gap-4">
               
